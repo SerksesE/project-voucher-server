99a7ef5cc8729e14c030349886f7f7473b58a68c
SerksesE <serkses@hotmail.com>
2018-12-18 17:16:55 +0100

    update

--- src/db.ts
+++ src/db.ts
@@ -47,12 +47,14 @@ export default () =>
         .then(location => {
           if (!location) {
             //if it does not exist we make a location named uva 
-            Location.create({ name: 'UvA', 
-            logo:'https://codaisseur-school.slack.com/files/UD4QG6WD6/FEW1KRFB3/enjoy_today_logo_white_transparent.png', 
-            background_color:'#F7F7F7', 
-            button_color:'#7EC683',
-            coffee_image:'https://codaisseur-school.slack.com/files/UD4QG6WD6/FEWR6L0JH/coffee_cup_icon.png',
-            coupon_image:'https://codaisseur-school.slack.com/files/UD4QG6WD6/FEY1QCPJS/coupon_-_empty.png' }).save()
+            Location.create({
+              name: 'UvA',
+              logo: 'https://www.dropbox.com/s/gcycga700r7ih5r/Enjoy%20Today%20logo%20white%20transparent.png?dl=1',
+              background_color: '#F7F7F7',
+              button_color: '#7EC683',
+              coffee_image: 'https://www.dropbox.com/s/ol19mjawhni4tm4/Coffee%20cup%20icon.png?dl=1',
+              coupon_image: 'https://www.dropbox.com/s/9kkyibw7ef7kdoa/Coupon%20-%20empty.png?dl=1'
+            }).save()
               .then(location => {
                 //checks if form already exists
                 Form.findOne({ typeform_id: 'hzCA6t' })
